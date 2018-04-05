2316429c9ed2dcf8f38326eaca4a16dc85460b6f
krlim417 <krlim417@gmail.com>
2018-04-05 13:17:03 -0700

    Optimizes bubbleSort
    Previously, the bubbleSort function would keep going through element of the array, disregarding the 'bubbled', sorted number(s) at the end of the list for each pass through. It would sort correctly, but would do this even if the passed in array was already sorted. This new bubbleSort function takes care of that with a while loop, which will only run once if the passed in array is sorted.

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,4 +1,24 @@
 function bubbleSort(arrayToSort) {
+  let isSorted = false;
+  let upperLimit = arrayToSort.length - 1;
+  while (isSorted === false) {
+    isSorted = true;
+    for (let i = 0; i < upperLimit; i++) {
+      if (arrayToSort[i] > arrayToSort[i + 1]) {
+        const temp = arrayToSort[i];
+        arrayToSort[i] = arrayToSort[i + 1];
+        arrayToSort[i + 1] = temp;
+        isSorted = false;
+      }
+    }
+    upperLimit--;
+  }
+  return arrayToSort;
+}
+
+/*
+
+function bubbleSort(arrayToSort) {
   for (let i = arrayToSort.length - 1; i > 0; i--) {
     for (let j = 0; j < i; j++) {
       if (arrayToSort[j] > arrayToSort[j + 1]) {
@@ -10,3 +30,5 @@ function bubbleSort(arrayToSort) {
   }
   return arrayToSort;
 }
+
+*/
